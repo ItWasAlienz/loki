2f6af0318a36b4d1b2506ca28283940efce6d9fe
It Was Alienz <34990467+ItWasAlienz@users.noreply.github.com>
2025-05-21 20:00:53 -0700

    Update voic.lua

--- lib/voic.lua
+++ lib/voic.lua
@@ -21,6 +21,8 @@ function pllpset(pll) ampll=pll if ampll<1 then clock.run(pollpaus,params:get("A
 function pollpaus(art) clock.sleep(0.04) params:set("ATr",art) end --..by setting amp-detection threshold, for 40ms,..
    --..at a value too high for detection(1), before going back to normal/specified threshold(to reduce false triggers)
 
+function recstut(lnth,num) clock.sync(lnth) params:set("V"..num.."_Rc",0) end --4 auto/short-length recording into stutter
+
 function delmap(num) return math.floor((num-1)/2)*116.0 end --'delay map': maps voice numbers to time in buffer for that..
  --..particular voice's delay-specific start of buffer(v1&2=0 v3&4=116 v5&6=232; +58=offset into buffer for stutter/looper)
  
@@ -28,26 +30,26 @@ function epos(indx,pstn)
   voices[indx].ennd[voices[indx].lpno]=pstn if voices[indx].lpno<8 then voices[indx].strt[voices[indx].lpno+1]=pstn end
 end
 
-function recstut(num,idx) clock.sync(num) params:set("V"..idx.."_Rc",0) end --4 auto/short-length recording into stutter
-
-function strtnd(num,lnth)
-  local strt,nd
+function strtndpos(num,lnth,ofst)
+  local strt,nd,pos
   local tmp=params:get("clock_tempo")
+  local qt=(60.0/tmp)
+  local lnthqt=qt*lnth
+  local ofstqt=qt*ofst
   if voices[num].mde==1 then
-    local qt=(60.0/tmp)
-    local lnthqt=qt*lnth
-    local phs=params:get("V"..num.."_Phase")
-    strt = voices[num].strt[voices[num].lpno]+(voices[num].prvstp*qt)+(phs*lnthqt) nd = strt+(lnthqt)
+    strt = util.wrap(voices[num].strt[voices[num].lpno]+ofstqt, voices[num].strt[voices[num].lpno], voices[num].ennd[voices[num].lpno]) 
+    nd = util.clamp(strt+(lnthqt), strt, delmap(num)+116.5)
   elseif voices[num].mde==3 then strt = voices[num].strt[voices[num].lpno] nd = voices[num].ennd[voices[num].lpno]
   else strt = delmap(num) nd = delmap(num)+(tmp*lnth) end
-  return strt,nd
+  pos = strt + (lnthqt*voices[num].phs)
+  return strt,nd,pos
 end
 
 function Voic:new(num)
   local v = setmetatable({}, { __index = Voic })
   v.num=num  v.mde=1 v.spd=1 v.vol=1 v.bar=8 v.looplay=0 v.busy=0 v.lpcount=1 v.prerec=0 v.pl=0 v.rc=0 v.pfreez=0 v.pg=0
-  v.lpno=1 v.ofst=0 v.tixx=1 v.prvstp=0 v.plf=0 v.llf=0 v.slf=0 v.flf=0 v.clf=0 v.qlf=0 v.ctf=1000 v.rq=10
-  v.fdri=1 v.flop=0 v.fhip=0 v.fbnp=0 v.fbnr=0
+  v.lpno=1 v.lnth=1 v.ofst=0 v.tixx=1 v.prvstp=0 v.plf=0 v.llf=0 v.slf=0 v.flf=0 v.clf=0 v.qlf=0 v.ctf=1000 v.rq=10
+  v.fdri=1 v.flop=0 v.fhip=0 v.fbnp=0 v.fbnr=0 v.fbk=0.0 v.cycbtz=8 v.phs=0 v.iptnz=3
   v.strt={delmap(num)+58,delmap(num)+58,delmap(num)+58,delmap(num)+58,delmap(num)+58,delmap(num)+58,delmap(num)+58,delmap(num)+58} 
   v.ennd={delmap(num)+116,delmap(num)+116,delmap(num)+116,delmap(num)+116,delmap(num)+116,delmap(num)+116,delmap(num)+116,delmap(num)+116} 
   softcut.enable(v.num,1) softcut.buffer(v.num,((v.num-1)%2)+1) softcut.loop(v.num,1) --setup softcut
@@ -56,69 +58,113 @@ function Voic:new(num)
   softcut.rate_slew_time(v.num,0.004) softcut.level_slew_time(v.num,0.01) softcut.recpre_slew_time(v.num,0.01)
   return v
 end
+                                 ----------------------------------------------
+                                   -- Softcut voices main trigger function --
+function Voic:sofxtrig(indx)
+  if self.pl>0 then
+    local phs=util.wrap((math.random(0,util.round(self.cycbtz*0.125,0.25)) + self.tixx)/self.cycbtz,0,1);
+    if self.pna>0 then softcut.pan(self.num,(math.random(-50,50)*0.02)*self.pna) end --(apply random panning)
+    if self.mde==3 then
+      if ((self.rc==0) and (self.iptnz~=0) and ((self.lpcount>self.iptnz) or (self.lpcount<0)) and (self.looplay>0) and (self.busy<1)) then
+        local rnd=math.random(2) local tmp=math.random(3,12)*2 local num=math.random(8,24) self.busy=1
+        clock.run(vstutz,phs,indx,rnd,tmp,num) --..improvise automated/randomized playback/stuttering
+      end
+    end
+  end  
+end
+
+                  -- Child/Spawned clock function for stutters applied on the softcut/looper voices --
+function vstutz(phs,vc,rnd,tmp,num)
+  local ofst=params:get("V"..vc.."_Ofst") local btsprbr=params:get("V"..vc.."_Cyc")
+  local ntlensc=15/params:get("clock_tempo")
+  for i=1,num do
+    if i>=(num-1) then voices[vc].busy=0
+    else
+      local tymex
+      if ((num>19) and (num<32)) then     --if incoming 'num'>10 do 'geometric series' style of stutter(accel/deccel)..
+        if rnd==2 then tymex = math.pow((1/tmp*2)*ntlensc,1.1) else tymex = math.pow(1/(tmp*3)*ntlensc,0.88) end clock.sleep(tymex)
+      elseif num<20 then                          --..otherwise do a regular stutter
+        if rnd==2 then tymex = 1/tmp*2 else tymex = 1/(tmp*4) end clock.sync(tymex)
+      end
+    end
+    params:set("V"..vc.."_Phase",phs)
+  end
+end
 
 -- Parameter-based functions
 function Voic:mode(mde) -- mode1 = stutter, mode2 = delay(echo), mode3 = looper
-  local ststrt, stend, lenth; lenth=params:get("V"..self.num.."_Len")
-  self.prvmde = self.mde self.lpcount=0 self.mde = mde ststrt,stend=strtnd(self.num,lenth)
+  local ststrt, stend, stpos;
+  self.prvmde = self.mde self.lpcount=0 self.mde = mde ststrt,stend,stpos=strtndpos(self.num,self.lnth,self.ofst)
   if self.mde==3 then 
-    softcut.rec(self.num,0) softcut.loop(self.num,1) softcut.position(self.num,self.strt[self.lpno])
+    softcut.rec(self.num,0) softcut.loop(self.num,1) softcut.position(self.num,stpos)
   else
     softcut.loop(self.num,1) 
     if self.mde==1 then self.prvstp=self.tixx params:set("V"..self.num.."_Phase",0.0) end
   end
-  softcut.pre_level(self.num,params:get("V"..self.num.."_Fbk")) softcut.position(self.num,ststrt)
+  softcut.pre_level(self.num,self.fbk) softcut.position(self.num,stpos)
   softcut.loop_start(self.num,ststrt) softcut.loop_end(self.num,stend)
 end
 
 function Voic:go(ply) -- play
-  local ststart,ennd=strtnd(self.num,params:get("V"..self.num.."_Len")) self.pl = ply
-  if self.mde == 3 then self.looplay=ply self.lpcount=1 end softcut.position(self.num,ststart) 
+  local ststart,ennd,posit=strtndpos(self.num, self.lnth, self.ofst) self.pl = ply
+  if self.mde == 3 then self.looplay=ply self.lpcount=1 end softcut.position(self.num,posit) 
   if self.mde ~= 2 then softcut.play(self.num,self.pl) else softcut.play(self.num,1) end
 end
 
 function Voic:rsync() self.tixx=tixx end
 
 function Voic:rec(rc) -- record
-  local tmp = 60.0/(params:get("clock_tempo")) local lenth = params:get("V"..self.num.."_Len")
-  local str,nd = strtnd(self.num,lenth) self.rc = rc
+  local tmp = 60.0/(params:get("clock_tempo"))
+  local str,nd,ps = strtndpos(self.num, self.lnth, self.ofst) self.rc = rc
   if self.mde==3 then
     local strtndx=self.strt[self.lpno]
     if rc>0 then 
-      pllpset(1) softcut.position(self.num,strtndx) self.lpcount=rc softcut.rec(self.num,rc) softcut.rec_level(self.num,rc)
+      pllpset(1) softcut.position(self.num,ps) self.lpcount=rc softcut.rec(self.num,rc) softcut.rec_level(self.num,rc)
     else softcut.query_position(self.num) softcut.rec_level(self.num,0) softcut.rec(self.num,0) end
   elseif self.mde==1 then
     if self.rc>0 then
-      softcut.rec(self.num,1) softcut.rec_level(self.num,1) clock.run(recstut,lenth*1.2,self.num) pllpset(1)
-      softcut.position(self.num,str-0.004) softcut.loop_start(self.num,str-0.004) softcut.loop_end(self.num,nd) 
+      softcut.rec(self.num,1) softcut.rec_level(self.num,1) clock.run(recstut,self.lnth*1.04,self.num) pllpset(1)
+      softcut.position(self.num,util.clamp(ps,delmap(self.num),delmap(self.num)+116))
+      softcut.loop_start(self.num,str) softcut.loop_end(self.num,nd) 
     else softcut.rec_level(self.num,0) softcut.rec(self.num,0) end
   else if self.rc>0 then pllpset(1) end softcut.rec(self.num,1) softcut.rec_level(self.num,self.rc) end
 end
 
+function Voic:cybtz(cycbtz) self.cycbtz = cycbtz end
+
+function Voic:lpnum(lpno) self.lpno = lpno end
+
+function Voic:ipatnz(iptnz) self.lpcount = 0 self.iptnz = iptnz end
+
 function Voic:length(lnth)   --only applicable to stutter(1) and looper(3) modes
-  local str,nd=strtnd(self.num,lnth)
-  softcut.loop_start(self.num,str) softcut.loop_end(self.num,nd)
+  local str,nd,ps=strtndpos(self.num,lnth,self.ofst)
+  self.lnth=lnth softcut.loop_start(self.num,str) softcut.loop_end(self.num,nd) softcut.position(self.num,ps)
+end
+
+function Voic:offset(ofst)
+  local str,nd,ps=strtndpos(self.num, self.lnth, ofst)
+  self.ofst=ofst softcut.loop_start(self.num,str) softcut.loop_end(self.num,nd) softcut.position(self.num,ps)
 end
 
 function Voic:lninsec(lnth)   --only applicable to stutter(1) and delay(2) modes
-  local offst local phs = params:get("V"..self.num.."_Phase")
+  local offst
   if self.mde==2 then offst=delmap(self.num)
-  elseif self.mde==1 then offst = delmap(self.num)+58.0+(phs*(self.ennd[self.lpno]-self.strt[self.lpno])) end
+  elseif self.mde==1 then offst = delmap(self.num)+58.0+(self.phs*(self.ennd[self.lpno]-self.strt[self.lpno])) end
   softcut.loop_start(self.num,offst)  softcut.loop_end(self.num,lnth+offst)
 end
 
 function Voic:phas(phs)    
-  local ststart,tmpo,lenth,btsprbr;  qtntsec=60.0/params:get("clock_tempo") ofst=params:get("V"..self.num.."_Ofst")
-  lenth=(qtntsec)*params:get("V"..self.num.."_Len")  btsprbr=params:get("V"..self.num.."_Cyc")
+  local ststart,lenth,qtntsec;  self.phs=phs
+  qtntsec=60.0/params:get("clock_tempo") lenth=(qtntsec)*self.lnth
   if self.mde==3 then
-    ststart=self.strt[self.lpno]+(qtntsec*ofst)+((qtntsec*(btsprbr-ofst))*phs)
+    ststart=self.strt[self.lpno]+(qtntsec*self.ofst)
   elseif self.mde==1 then
     local rootphs=self.prvstp
-    ststart = self.strt[self.lpno]+(qtntsec*rootphs)+(phs*lenth)
+    ststart = self.strt[self.lpno]+(qtntsec*rootphs)+(self.phs*lenth)
     softcut.loop_start(self.num,ststart)  softcut.loop_end(self.num,ststart+lenth)
   else 
     ststart,btsprbr=delmap(self.num) 
-    ststart=ststart+(phs*lenth)
+    ststart=ststart+(self.phs*lenth)
     softcut.loop_start(self.num,ststart)  
     softcut.loop_end(self.num,ststart+lenth) 
   end
@@ -128,6 +174,10 @@ end
 function Voic:speed(spd) self.spd = spd softcut.rate(self.num,self.spd) end
 
 function Voic:gain(vol) self.vol = vol softcut.level(self.num,self.vol) end
+
+function Voic:pnabausch(pan) self.pna = pan end
+
+function Voic:fdbk(vol) self.fbk = vol softcut.pre_level(self.num,self.fbk) end
 
 function Voic:cutoff(ctoff) self.ctf = ctoff softcut.post_filter_fc(self.num,self.ctf) end
 
@@ -255,3 +305,5 @@ function Voic:inputselect(inz, itbl)
     softcut.level_cut_cut(itabl[5],self.num,0.0)
   end
 end
+
+voices={} for i=1,6 do table.insert(voices,Voic:new(i)) end
